01d824d7ce32c5c436aad4e1a278cc07a1b13652
Michał Guz <michal.guz.85@gmail.com>
2023-12-21 10:08:44 +0100

    Creating a table

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-import './App.css';
-import { Calendar } from 'rsuite';
-import 'rsuite/dist//rsuite.min.css';
+import "./App.css";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Calendar></Calendar>
-      </header>
-    </div>
-  );
+function createData(name, nick, number, team, score) {
+  return { name, nick, number, team, score };
 }
 
-export default App;
+const rows = [createData()];
+
+export default function BasicTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="left">Turnament date</TableCell>
+            <TableCell align="left">League</TableCell>
+            <TableCell align="left">PDGA</TableCell>
+            <TableCell align="left">Turnament name</TableCell>
+            <TableCell align="left">Registration</TableCell>
+            <TableCell align="left">State</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.date}</TableCell>
+              <TableCell align="right">{row.pdga}</TableCell>
+              <TableCell align="right">{row.name}</TableCell>
+              <TableCell align="right">{row.registration}</TableCell>
+              <TableCell align="right">{row.state}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
