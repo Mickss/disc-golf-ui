ed00f74d22219f5701c3d75e669d04f463460136
Michał Guz <michal.guz.85@gmail.com>
2023-12-21 23:55:19 +0100

    Correcting the data in the table

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
-function createData(name, nick, number, team, score) {
-  return { name, nick, number, team, score };
+function createData(date, league, pdga, title, registration, state) {
+  return { date, league, pdga, title, registration, state };
 }
 
-const rows = [createData()];
+const rows = [
+  createData("05/03/2024", 159, "-", "Fire Disk", "Phase 1", '22 / 22'),
+  createData("15/06/2024", 237, "-", "Spin Around", "Phase 1", '0 / 30'),
+  createData("04/08/2024", 262, "-", "Golf Basket", "Phase 1", '33 / 45'),
+  createData("09/10/2024", 305, "-", "Flaying plates", "Phase 1", '20 / 25'),
+  createData("11/11/2024", 356, "-", "Team Disk", "Phase 1", '22 / 26'),
+];
 
 export default function BasicTable() {
   return (
@@ -22,7 +28,7 @@ export default function BasicTable() {
             <TableCell align="left">Turnament date</TableCell>
             <TableCell align="left">League</TableCell>
             <TableCell align="left">PDGA</TableCell>
-            <TableCell align="left">Turnament name</TableCell>
+            <TableCell align="left">Turnament title</TableCell>
             <TableCell align="left">Registration</TableCell>
             <TableCell align="left">State</TableCell>
           </TableRow>
@@ -33,14 +39,12 @@ export default function BasicTable() {
               key={row.name}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.date}</TableCell>
-              <TableCell align="right">{row.pdga}</TableCell>
-              <TableCell align="right">{row.name}</TableCell>
-              <TableCell align="right">{row.registration}</TableCell>
-              <TableCell align="right">{row.state}</TableCell>
+              <TableCell align="left">{row.date}</TableCell>
+              <TableCell align="left">{row.league}</TableCell>
+              <TableCell align="left">{row.pdga}</TableCell>
+              <TableCell align="left">{row.title}</TableCell>
+              <TableCell align="left">{row.registration}</TableCell>
+              <TableCell align="left">{row.state}</TableCell>
             </TableRow>
           ))}
         </TableBody>
