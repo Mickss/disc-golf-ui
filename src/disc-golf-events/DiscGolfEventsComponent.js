63215cef37f52a1b6d30b0b0de750b6e76f469b8
Michał Guz <michal.guz.85@gmail.com>
2024-01-13 16:44:20 +0100

    Removed conlole log and brackets

--- src/disc-golf-events/DiscGolfEventsComponent.js
+++ src/disc-golf-events/DiscGolfEventsComponent.js
@@ -9,49 +9,48 @@ import Paper from "@mui/material/Paper";
 
 const DiscGolfEventsComponent = () => {
 
-      const [discGolfEvents, setDiscGolfEvents] = useState([]);
+    const [discGolfEvents, setDiscGolfEvents] = useState([]);
 
-      useEffect(() => {
+    useEffect(() => {
         fetchEvents();
-      }, []);
+    }, []);
 
-      const fetchEvents = () => {
-        console.log("Fetching now");
+    const fetchEvents = () => {
         fetch("http://localhost:8080/events")
-          .then((response) => response.json())
-          .then((data) => setDiscGolfEvents(data));
-      };
+            .then(response => response.json())
+            .then(data => setDiscGolfEvents(data));
+    };
 
     return (
-    <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="left">Turnament date</TableCell>
-              <TableCell align="left">League</TableCell>
-              <TableCell align="left">PDGA</TableCell>
-              <TableCell align="left">Turnament title</TableCell>
-              <TableCell align="left">Registration</TableCell>
-              <TableCell align="left">State</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {discGolfEvents.map((discGolfEvent) => (
-              <TableRow
-                key={discGolfEvent.name}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell align="left">{discGolfEvent.tournamentDate}</TableCell>
-                <TableCell align="left">{discGolfEvent.league}</TableCell>
-                <TableCell align="left">{discGolfEvent.pdga}</TableCell>
-                <TableCell align="left">{discGolfEvent.tournamentTitle}</TableCell>
-                <TableCell align="left">{discGolfEvent.registration}</TableCell>
-                <TableCell align="left">{discGolfEvent.state}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell align="left">Turnament date</TableCell>
+                        <TableCell align="left">League</TableCell>
+                        <TableCell align="left">PDGA</TableCell>
+                        <TableCell align="left">Turnament title</TableCell>
+                        <TableCell align="left">Registration</TableCell>
+                        <TableCell align="left">State</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {discGolfEvents.map(discGolfEvent => (
+                        <TableRow
+                            key={discGolfEvent.name}
+                            sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                        >
+                            <TableCell align="left">{discGolfEvent.tournamentDate}</TableCell>
+                            <TableCell align="left">{discGolfEvent.league}</TableCell>
+                            <TableCell align="left">{discGolfEvent.pdga}</TableCell>
+                            <TableCell align="left">{discGolfEvent.tournamentTitle}</TableCell>
+                            <TableCell align="left">{discGolfEvent.registration}</TableCell>
+                            <TableCell align="left">{discGolfEvent.state}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
 
