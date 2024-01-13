cf8c0f55256070e577df9ebfed5cc74e22a71343
Michał Guz <michal.guz.85@gmail.com>
2024-01-13 16:27:12 +0100

    Calling backend for events

--- src/App.js
+++ src/App.js
@@ -1,54 +1,8 @@
 import "./App.css";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-
-function createData(date, league, pdga, title, registration, state) {
-  return { date, league, pdga, title, registration, state };
-}
-
-const rows = [
-  createData("05/03/2024", 159, "-", "Fire Disk", "Phase 1", '22 / 22'),
-  createData("15/06/2024", 237, "-", "Spin Around", "Phase 1", '0 / 30'),
-  createData("04/08/2024", 262, "-", "Golf Basket", "Phase 1", '33 / 45'),
-  createData("09/10/2024", 305, "-", "Flaying plates", "Phase 1", '20 / 25'),
-  createData("11/11/2024", 356, "-", "Team Disk", "Phase 1", '22 / 26'),
-];
+import DiscGolfEventsComponent from "./disc-golf-events/DiscGolfEventsComponent";
 
 export default function BasicTable() {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="left">Turnament date</TableCell>
-            <TableCell align="left">League</TableCell>
-            <TableCell align="left">PDGA</TableCell>
-            <TableCell align="left">Turnament title</TableCell>
-            <TableCell align="left">Registration</TableCell>
-            <TableCell align="left">State</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell align="left">{row.date}</TableCell>
-              <TableCell align="left">{row.league}</TableCell>
-              <TableCell align="left">{row.pdga}</TableCell>
-              <TableCell align="left">{row.title}</TableCell>
-              <TableCell align="left">{row.registration}</TableCell>
-              <TableCell align="left">{row.state}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <DiscGolfEventsComponent></DiscGolfEventsComponent>
   );
 }
---
+++ src/disc-golf-events/DiscGolfEventsComponent.js
@@ -0,0 +1,58 @@
+import { useEffect, useState, useRef } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+const DiscGolfEventsComponent = () => {
+
+      const [discGolfEvents, setDiscGolfEvents] = useState([]);
+
+      useEffect(() => {
+        fetchEvents();
+      }, []);
+
+      const fetchEvents = () => {
+        console.log("Fetching now");
+        fetch("http://localhost:8080/events")
+          .then((response) => response.json())
+          .then((data) => setDiscGolfEvents(data));
+      };
+
+    return (
+    <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="left">Turnament date</TableCell>
+              <TableCell align="left">League</TableCell>
+              <TableCell align="left">PDGA</TableCell>
+              <TableCell align="left">Turnament title</TableCell>
+              <TableCell align="left">Registration</TableCell>
+              <TableCell align="left">State</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {discGolfEvents.map((discGolfEvent) => (
+              <TableRow
+                key={discGolfEvent.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell align="left">{discGolfEvent.tournamentDate}</TableCell>
+                <TableCell align="left">{discGolfEvent.league}</TableCell>
+                <TableCell align="left">{discGolfEvent.pdga}</TableCell>
+                <TableCell align="left">{discGolfEvent.tournamentTitle}</TableCell>
+                <TableCell align="left">{discGolfEvent.registration}</TableCell>
+                <TableCell align="left">{discGolfEvent.state}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    )
+}
+
+export default DiscGolfEventsComponent;
