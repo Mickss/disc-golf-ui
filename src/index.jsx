5d8d5c4cb0d650cffabd7bb2e0658ca5a0aa2c62
Tomasz Nowak <nowaktomasz2@gmail.com>
2025-02-27 10:05:04 +0100

    DG-59 Migrate React to v19. Vite added.

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from "./auth/AuthContext";
-import reportWebVitals from './reportWebVitals';
 import {LoadingProvider} from "./spinner/LoadingProvider";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -19,8 +18,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
