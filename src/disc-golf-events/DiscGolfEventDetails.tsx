fb576026fb346821515eedd759083039a7ad12ee
Michał Guz <michal.guz.85@gmail.com>
2025-12-21 13:34:32 +0100

    DG-142 Code improvements and fixes.

--- src/disc-golf-events/DiscGolfEventDetails.tsx
+++ src/disc-golf-events/DiscGolfEventDetails.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { Container, Typography, Paper, Button} from '@mui/material';
+import { Container, Typography, Paper, Button } from '@mui/material';
 import { DiscGolfEvent } from './DiscGolfEvent';
 import { useLoading } from '../spinner/LoadingProvider';
 import StatusMessage from '../components/StatusMessage';
@@ -15,10 +15,12 @@ const DiscGolfEventDetails = () => {
 
     useEffect(() => {
         const fetchEventDetails = async () => {
+            if (!id) return;
+
             setLoading(true);
             setError('');
             try {
-                const data = await EventService.getEventById(id!);
+                const data = await EventService.getEventById(id);
                 setEvent(data);
             } catch (err: any) {
                 console.error('Tournament fetching error:', err);
@@ -28,63 +30,63 @@ const DiscGolfEventDetails = () => {
             }
         };
 
-        if (id) {
-            fetchEventDetails();
-        }
+        fetchEventDetails();
     }, [id, setLoading]);
 
     if (error) {
         return <StatusMessage severity="error" message={error} />;
     }
 
-    if (!event) return null;
-
     return (
-        <Container maxWidth="md" sx={{ mt: 4, mb: 4 }}>
-            <Button
-                variant="outlined"
-                onClick={() => navigate('/events')}
-                sx={{ mb: 2 }}
-            >
-                ⬅️ Back to Events
-            </Button>
+        <>
+            {event && (
+                <Container maxWidth="md" sx={{ mt: 4, mb: 4 }}>
+                    <Button
+                        variant="outlined"
+                        onClick={() => navigate('/events')}
+                        sx={{ mb: 2 }}
+                    >
+                        ⬅️ Back to Events
+                    </Button>
 
-            <Paper elevation={3} sx={{ p: 4 }}>
-                <Typography variant="h4" gutterBottom>
-                    {event.tournamentTitle}
-                </Typography>
+                    <Paper elevation={3} sx={{ p: 4 }}>
+                        <Typography variant="h4" gutterBottom>
+                            {event.tournamentTitle}
+                        </Typography>
 
-                <Typography variant="h6" gutterBottom>
-                    Region: {event.region}
-                </Typography>
+                        <Typography variant="h6" gutterBottom>
+                            Region: {event.region}
+                        </Typography>
 
-                <Typography variant="body1" sx={{ mt: 2 }}>
-                    Tournament date: <strong>{new Date(event.tournamentDate).toLocaleDateString('pl-PL')}</strong>
-                </Typography>
+                        <Typography variant="body1" sx={{ mt: 2 }}>
+                            Tournament date: <strong>{new Date(event.tournamentDate).toLocaleDateString('pl-PL')}</strong>
+                        </Typography>
 
-                {event.pdga && (
-                    <Typography variant="body1" sx={{ mt: 2 }}>
-                        PDGA: {event.pdga}
-                    </Typography>
-                )}
-
-                {event.externalLink && (
-                    <div style={{ marginTop: '16px' }}>
-                        {event.externalLink.split(';').filter(Boolean).map((link, index, array) => (
-                            <Typography key={index} variant="body2" sx={{ mt: 1 }}>
-                                <a
-                                    href={link}
-                                    target="_blank"
-                                    style={{ color: '#1976d2', textDecoration: 'none' }}
-                                >
-                                    Tournament website{array.length > 1 ? ` ${index + 1}` : ''} ➡️
-                                </a>
+                        {event.pdga && (
+                            <Typography variant="body1" sx={{ mt: 2 }}>
+                                PDGA: {event.pdga}
                             </Typography>
-                        ))}
-                    </div>
-                )}
-            </Paper>
-        </Container>
+                        )}
+
+                        {event.externalLink && (
+                            <div style={{ marginTop: '16px' }}>
+                                {event.externalLink.split(';').map((link, index, array) => (
+                                    <Typography key={index} variant="body2" sx={{ mt: 1 }}>
+                                        <a
+                                            href={link}
+                                            target="_blank"
+                                            style={{ color: '#1976d2', textDecoration: 'none' }}
+                                        >
+                                            Tournament website{array.length > 1 ? ` ${index + 1}` : ''} ➡️
+                                        </a>
+                                    </Typography>
+                                ))}
+                            </div>
+                        )}
+                    </Paper>
+                </Container>
+            )}
+        </>
     );
 };
 
